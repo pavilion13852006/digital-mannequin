631442bc17446e14505d52ad5cea722f41060d5a
pavilion13852006 <130180169+pavilion13852006@users.noreply.github.com>
2025-09-20 10:40:55 +0330

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer: React.FC = () => {
     <footer className="bg-gray-800/50 text-center py-4">
       <div className="container mx-auto px-4">
         <p className="text-sm text-gray-500">
-          &copy; {new Date().getFullYear()} Digital Mannequin AI. Powered by Google Gemini.
+          &copy; {new Date().getFullYear()} Digital Mannequin AI. Powered by seyed saifollah hoseini.
         </p>
       </div>
     </footer>
